6b337a55b2eccf98a53d05d1d369bbfb0829dbce
Muhammad Hazrat <221441166@formanite.fccollege.edu.pk>
2022-02-06 13:24:58 +0500

    nothing

--- Database/server_configuration.php
+++ Database/server_configuration.php
@@ -1,8 +1,16 @@
 <?php
 
-$host="localhost";
-$username="root";
-$passwrd="";
+    $host="localhost";
+    $username="root";
+    $passwrd="";
+
+    $link = mysqli_connect($host, $username, $passwrd);
+
+    if (@mysqli_connect_error()) {
+        die('User cannot connect to the database.... Try Again!');
+    } else {
+        @mysqli_select_db($link, 'database name');
+    }
 
 
 
