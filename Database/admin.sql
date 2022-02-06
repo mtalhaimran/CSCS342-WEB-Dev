d3ffe0bb700a65e3148ce1028780dd449ba68402
Muhammad Hazrat <221441166@formanite.fccollege.edu.pk>
2022-02-06 11:55:43 +0500

    added admin.sql and seller.sql

---
+++ Database/admin.sql
@@ -0,0 +1,44 @@
+/*script generated for free with OnlineDataGenerator available at: https://www.onlinedatagenerator.com */
+CREATE TABLE admin(id BIGINT,fullName VARCHAR(500),username VARCHAR(500),password VARCHAR(500),address VARCHAR(500),age INT,email VARCHAR(500),phoneNumber VARCHAR(500));
+
+INSERT INTO admin VALUES('1','Tyson Latham','Tyson','Y5Bv1lQJTm','Thorndike   Walk, 352',89,'Tyson_Latham152@nanoff.biz','7-010-788-8303');
+INSERT INTO admin VALUES('2','Tom Drake','Tom','neGZPyGyR6','Falconberg   Boulevard, 6060',49,'Tom_Drake8491@brety.org','6-661-662-3230');
+INSERT INTO admin VALUES('3','Mina Hobson','Mina','KnMN86pmmM','Birkin   Road, 8060',34,'Mina_Hobson1647@cispeto.com','3-415-368-7381');
+INSERT INTO admin VALUES('4','Hank Thomson','Hank','yDbawPHYZ3','Birkin   Alley, 561',58,'Hank_Thomson996@nickia.com','8-016-048-6436');
+INSERT INTO admin VALUES('5','Denis Duvall','Denis','u5awXAlXZ4','Durnford  Drive, 1371',35,'Denis_Duvall5966@jiman.org','6-466-804-4731');
+INSERT INTO admin VALUES('6','Daniel Penn','Daniel','hriUJImQHF','King Hill, 6711',41,'Daniel_Penn3429@famism.biz','2-612-626-1053');
+INSERT INTO admin VALUES('7','Ramon Taylor','Ramon','32cCXpaxEc','Abourne   Grove, 3334',67,'Ramon_Taylor6241@joiniaa.com','3-216-035-6628');
+INSERT INTO admin VALUES('8','Alan Flanders','Alan','vpRFLMGn4p','Carolina  Avenue, 6518',72,'Alan_Flanders5944@jiman.org','8-146-117-2842');
+INSERT INTO admin VALUES('9','Karla Glynn','Karla','sBjiq8IOZa','Bloomsbury  Walk, 3509',55,'Karla_Glynn1314@cispeto.com','6-803-378-4257');
+INSERT INTO admin VALUES('10','Rita Bristow','Rita','5URy7k6F8l','Berriman  Rue, 7283',25,'Rita_Bristow3731@joiniaa.com','6-304-636-8681');
+INSERT INTO admin VALUES('11','Alessandra Verdon','Alessandra','JFHue9ggfh','Parkgate  Rue, 9638',56,'Alessandra_Verdon2933@nimogy.biz','0-717-738-5855');
+INSERT INTO admin VALUES('12','David Cox','David','c5y5cCi9kY','Queen Tunnel, 8841',64,'David_Cox9124@sveldo.biz','3-250-727-3161');
+INSERT INTO admin VALUES('13','Melanie Cartwright','Melanie','WKabEnzCBP','Blackheath  Alley, 2089',82,'Melanie_Cartwright9909@iatim.tech','4-367-236-0210');
+INSERT INTO admin VALUES('14','Mandy Benfield','Mandy','4DJqBsybeR','Geffrye   Boulevard, 2106',49,'Mandy_Benfield9038@deavo.com','1-035-166-0684');
+INSERT INTO admin VALUES('15','Maxwell Archer','Maxwell','hYSRVE7gvQ','Bush  Alley, 9028',54,'Maxwell_Archer5114@guentu.biz','1-214-000-1512');
+INSERT INTO admin VALUES('16','Liv Latham','Liv','PbOiu2B5Cz','Cave   Way, 2890',28,'Liv_Latham7403@famism.biz','7-247-611-5843');
+INSERT INTO admin VALUES('17','Julian Saunders','Julian','v1gqBW2Vrm','Bennett  Tunnel, 2698',44,'Julian_Saunders7914@joiniaa.com','6-834-385-1202');
+INSERT INTO admin VALUES('18','Martin Parr','Martin','g4M3E3EkGW','Waite  Avenue, 6350',92,'Martin_Parr9169@cispeto.com','7-774-636-1772');
+INSERT INTO admin VALUES('19','Domenic Foxley','Domenic','HSrbG7EXQ3','Buttonwood Vale, 8109',23,'Domenic_Foxley3282@guentu.biz','2-527-136-1402');
+INSERT INTO admin VALUES('20','Barney Oldfield','Barney','lwMLP3wLx7','Thorndike   Avenue, 6134',97,'Barney_Oldfield1707@mafthy.com','5-578-450-5665');
+INSERT INTO admin VALUES('21','Tom Moore','Tom','tERASi6mzb','Sherlock   Vale, 8563',46,'Tom_Moore3541@deavo.com','6-688-450-1687');
+INSERT INTO admin VALUES('22','Peter Giles','Peter','TMOwNWNlpm','Cobden  Road, 7845',54,'Peter_Giles8087@ubusive.com','6-528-875-0518');
+INSERT INTO admin VALUES('23','Wade Russel','Wade','wVJUH33EI9','Bolton  Vale, 845',46,'Wade_Russel2667@kideod.biz','7-533-113-4445');
+INSERT INTO admin VALUES('24','Wade Drake','Wade','XHXUzS1cU4','Udall   Pass, 5196',38,'Wade_Drake5075@qater.org','6-260-236-7158');
+INSERT INTO admin VALUES('25','Rocco Chapman','Rocco','Rr0CmXvuf8','Caroline  Route, 3250',55,'Rocco_Chapman5@corti.com','5-185-633-1703');
+INSERT INTO admin VALUES('26','Hank Logan','Hank','TLnqmt1dnI','Clarges   Drive, 7443',83,'Hank_Logan5090@bulaffy.com','0-275-076-1782');
+INSERT INTO admin VALUES('27','Alan Saunders','Alan','9LjUoZunzQ','Clere  Vale, 4792',86,'Alan_Saunders5879@sheye.org','5-558-017-4134');
+INSERT INTO admin VALUES('28','Kurt Wright','Kurt','cIPDJ3ZZJG','Yorkshire  Route, 4323',61,'Kurt_Wright449@bungar.biz','3-751-351-3473');
+INSERT INTO admin VALUES('29','Maxwell Mullins','Maxwell','4RzJxe2aqr','Glenwood Street, 4797',59,'Maxwell_Mullins7267@muall.tech','6-241-057-6370');
+INSERT INTO admin VALUES('30','Erica Stevenson','Erica','eVO5M2hRsi','Oxford Drive, 1640',90,'Erica_Stevenson7733@atink.com','1-557-115-8123');
+INSERT INTO admin VALUES('31','Erick Powell','Erick','d94YMA7iT0','St. Jamess  Route, 5441',92,'Erick_Powell1795@dionrab.com','4-081-532-5136');
+INSERT INTO admin VALUES('32','Emerald Walker','Emerald','dWdtFDDvpv','Balham   Way, 2983',81,'Emerald_Walker7591@sheye.org','5-670-732-5306');
+INSERT INTO admin VALUES('33','Jayden Harper','Jayden','OrwXXu6xyH','Maple Pass, 3275',28,'Jayden_Harper9992@yahoo.com','1-473-704-6100');
+INSERT INTO admin VALUES('34','Christine London','Christine','Vwd0gez5YA','Bacton   Walk, 8573',72,'Christine_London663@dionrab.com','0-152-813-8012');
+INSERT INTO admin VALUES('35','Wade Smith','Wade','oFZwwPOdlf','Addison  Street, 1116',88,'Wade_Smith5339@yahoo.com','2-750-082-0013');
+INSERT INTO admin VALUES('36','Rosa Shepherd','Rosa','OtrGWtWE3y','Bacton   Way, 5352',26,'Rosa_Shepherd1166@bauros.biz','0-438-844-1267');
+INSERT INTO admin VALUES('37','Danny Welsch','Danny','2qnORceAfx','Bendall   Vale, 9041',82,'Danny_Welsch2670@joiniaa.com','3-182-071-7362');
+INSERT INTO admin VALUES('38','Norah Cork','Norah','3CWQ6I7Szo','Apostle  Alley, 454',67,'Norah_Cork3154@naiker.biz','4-677-263-3504');
+INSERT INTO admin VALUES('39','Eve Robertson','Eve','O4My3nwn0H','Angela   Vale, 3134',44,'Eve_Robertson619@extex.org','6-545-246-8416');
+INSERT INTO admin VALUES('40','Carina Radcliffe','Carina','mJfTZW2Orq','Chambers  Lane, 8529',78,'Carina_Radcliffe4293@sveldo.biz','3-025-282-1261');
+
---
+++ Database/seller.sql
@@ -0,0 +1,44 @@
+/*script generated for free with OnlineDataGenerator available at: https://www.onlinedatagenerator.com */
+CREATE TABLE seller(id BIGINT,fullName VARCHAR(500),username VARCHAR(500),password VARCHAR(500),address VARCHAR(500),age INT,email VARCHAR(500),phoneNumber VARCHAR(500));
+
+INSERT INTO seller VALUES('1','Nicholas Snell','Nicholas','8FfI8RT3X4','Canning  Walk, 6380',26,'Nicholas_Snell7301@sveldo.biz','2-345-230-0176');
+INSERT INTO seller VALUES('2','Domenic Noon','Domenic','nnNGml5Ann','Cephas  Street, 2977',20,'Domenic_Noon2618@nanoff.biz','0-412-857-6350');
+INSERT INTO seller VALUES('3','Kate Truscott','Kate','kvfuQ8igdH','Waite  Tunnel, 7070',90,'Kate_Truscott8624@yahoo.com','0-878-713-1606');
+INSERT INTO seller VALUES('4','Julian Chadwick','Julian','gd9dDAsivR','Norfolk  Avenue, 794',18,'Julian_Chadwick859@nickia.com','7-073-406-0333');
+INSERT INTO seller VALUES('5','Colleen Clifford','Colleen','OAxclrPOz0','Sundown Avenue, 2155',58,'Colleen_Clifford3412@jiman.org','0-300-166-3437');
+INSERT INTO seller VALUES('6','Charlotte James','Charlotte','oVhiDlsmYX','Arbutus   Way, 1793',96,'Charlotte_James397@fuliss.net','7-161-382-8842');
+INSERT INTO seller VALUES('7','Nancy Mason','Nancy','4TINM88obt','Lonsdale  Grove, 39',72,'Nancy_Mason6508@gompie.com','5-724-451-8260');
+INSERT INTO seller VALUES('8','Destiny Pearson','Destiny','URpNdGe7uE','Maple Street, 7251',98,'Destiny_Pearson5724@bungar.biz','8-523-840-4520');
+INSERT INTO seller VALUES('9','Eileen Lomax','Eileen','7XF3ZQvxLi','Lexington Way, 319',39,'Eileen_Lomax4713@twipet.com','8-232-632-1571');
+INSERT INTO seller VALUES('10','George Norman','George','9U9OHnJziS','Wakley   Vale, 4750',79,'George_Norman3379@ubusive.com','4-783-376-5750');
+INSERT INTO seller VALUES('11','Brad Price','Brad','GjO9iulxIz','Carpenter Alley, 6712',26,'Brad_Price1506@gompie.com','7-572-321-1715');
+INSERT INTO seller VALUES('12','Julianna Flanders','Julianna','iNj8Mk5dLk','Hampden  Rue, 7087',30,'Julianna_Flanders2127@bretoux.com','1-541-641-8173');
+INSERT INTO seller VALUES('13','Doris Overson','Doris','KQUnR4eq3i','Cobden  Route, 3668',36,'Doris_Overson5427@deavo.com','5-084-887-2600');
+INSERT INTO seller VALUES('14','Shelby Gordon','Shelby','O3TvaeUE2S','Rail Avenue, 7553',95,'Shelby_Gordon5617@acrit.org','1-834-120-5423');
+INSERT INTO seller VALUES('15','Liam Page ','Liam','y35dUuFlxD','Durnford  Hill, 1773',75,'Liam_Page 7057@elnee.tech','1-763-711-2287');
+INSERT INTO seller VALUES('16','Monica Lowe','Monica','DPMld4kRmy','Boldero   Hill, 905',48,'Monica_Lowe1247@muall.tech','1-380-043-4356');
+INSERT INTO seller VALUES('17','Elisabeth Robinson','Elisabeth','2aUm9IVjNw','Howard Lane, 2567',89,'Elisabeth_Robinson1637@bretoux.com','1-715-746-1047');
+INSERT INTO seller VALUES('18','Wade Needham','Wade','L5BN5zOcX9','Beaumont  Hill, 1331',30,'Wade_Needham2333@nanoff.biz','2-420-484-5536');
+INSERT INTO seller VALUES('19','Clint Patel','Clint','RkVUl41C6U','Lexington Rue, 4091',30,'Clint_Patel2793@gembat.biz','3-488-234-1323');
+INSERT INTO seller VALUES('20','Harry Radley','Harry','GG63ssOxhT','Underwood  Tunnel, 5751',96,'Harry_Radley9941@typill.biz','4-333-811-5281');
+INSERT INTO seller VALUES('21','Bethany Gray','Bethany','ffbXN8rgbi','Wake  Hill, 25',71,'Bethany_Gray6289@infotech44.tech','6-415-608-1588');
+INSERT INTO seller VALUES('22','Martin Potter','Martin','kC3KyxyLtB','Enderby   Road, 9332',85,'Martin_Potter4245@elnee.tech','1-482-150-2644');
+INSERT INTO seller VALUES('23','Rocco Emmott','Rocco','BV2p86qz0z','Wakley   Hill, 7836',29,'Rocco_Emmott4930@nickia.com','0-161-633-3580');
+INSERT INTO seller VALUES('24','Claire Yoman','Claire','RjWb9vIQ0d','Hampden  Boulevard, 6697',28,'Claire_Yoman1215@vetan.org','7-870-334-6458');
+INSERT INTO seller VALUES('25','Leilani Grady','Leilani','6GVfhkMVIt','Castile  Crossroad, 3175',41,'Leilani_Grady3962@elnee.tech','6-228-106-4880');
+INSERT INTO seller VALUES('26','Carter Fulton','Carter','zlL1hn9Kkg','Coalecroft  Avenue, 7616',25,'Carter_Fulton4813@womeona.net','0-120-752-0773');
+INSERT INTO seller VALUES('27','Meredith Evans','Meredith','fgZs35nctu','Bocking   Way, 6932',75,'Meredith_Evans5751@nickia.com','3-683-883-7512');
+INSERT INTO seller VALUES('28','Gabriel Oliver','Gabriel','SGn9B3FCLh','Queen Hill, 4536',61,'Gabriel_Oliver6368@gembat.biz','8-541-468-0651');
+INSERT INTO seller VALUES('29','Carl Maxwell','Carl','ohaBcU8rpq','Dyott   Boulevard, 4788',54,'Carl_Maxwell5423@ubusive.com','4-713-165-7251');
+INSERT INTO seller VALUES('30','Emery Overson','Emery','vg4c3VIaES','Central  Tunnel, 9382',86,'Emery_Overson5976@nanoff.biz','1-453-123-6488');
+INSERT INTO seller VALUES('31','Mike Wilcox','Mike','HbYP4DYAhL','Clarks  Road, 7887',92,'Mike_Wilcox1467@infotech44.tech','6-620-368-8241');
+INSERT INTO seller VALUES('32','Doug Morris','Doug','YinPLx1q4h','West Street, 9066',48,'Doug_Morris1505@atink.com','4-354-213-6334');
+INSERT INTO seller VALUES('33','Aleksandra Warner','Aleksandra','3ODlxnWhsV','Enderby   Road, 8315',76,'Aleksandra_Warner2828@eirey.tech','7-571-065-3611');
+INSERT INTO seller VALUES('34','Melanie Thatcher','Melanie','UargohISjf','West Lane, 7192',38,'Melanie_Thatcher4305@nanoff.biz','3-541-475-7315');
+INSERT INTO seller VALUES('35','Noah Gosling','Noah','acr2AqVSDt','Arlington  Hill, 403',42,'Noah_Gosling3441@atink.com','0-284-754-6347');
+INSERT INTO seller VALUES('36','Maxwell Gordon','Maxwell','2fY6FTN0Ou','Edwin   Alley, 5687',76,'Maxwell_Gordon580@bulaffy.com','7-241-125-3778');
+INSERT INTO seller VALUES('37','Vera Lloyd','Vera','3fHi0lKFNB','Thoresby   Vale, 958',38,'Vera_Lloyd6955@brety.org','0-654-542-8357');
+INSERT INTO seller VALUES('38','Rachael Vangness','Rachael','UKUq0U7A1c','Clissold  Pass, 1150',58,'Rachael_Vangness2637@infotech44.tech','2-360-723-0831');
+INSERT INTO seller VALUES('39','Bristol Smith','Bristol','tK78quzZYW','West Road, 9029',31,'Bristol_Smith7204@bulaffy.com','3-763-242-0306');
+INSERT INTO seller VALUES('40','Mike Sanchez','Mike','J0xisjvJyf','Black Friars   Crossroad, 9874',43,'Mike_Sanchez3055@typill.biz','2-228-033-7827');
+
