ebbe6e1ffd9513dad27bd4dc1f23dd3c0e3cf3be
Muhammad Talha Imran <92talhaimran@gmail.com>
2022-02-08 09:43:23 +0500

    main_admin.php changed to admin_panel.php

--- index.php
+++ index.php
@@ -1,79 +1,78 @@
 <!DOCTYPE html>
 <html lang="eng">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Hazrat & Talha">
     <link rel="icon" href="favicon.ico">
-
     <title>Admin CMS Login</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <style>
-		html, body {
-			height: 100%;
-}
-		body {
-			background-image: url(img/bg_1.jpg);
-			/* background-size: auto; */
-			background-position:center;
-      background-size:cover;
-}
-		@font-face {
-			font-family: workSans;
-			src: url(font/WorkSans-ExtraLight.ttf);
-		}
-		ul li a {
-			font-size: 17px;
-			font-family: "workSans";
-			color: rgb(255, 255, 255);
-			font-weight: bold;
-		}
+        html, body {
+            height: 100%;
+        }
+        body {
+            background-image: url(img/bg_1.jpg);
+            /* background-size: auto; */
+            background-position:center;
+            background-size:cover;
+        }
+        @font-face {
+            font-family: workSans;
+            src: url(font/WorkSans-ExtraLight.ttf);
+        }
+        ul li a {
+            font-size: 17px;
+            font-family: "workSans";
+            color: rgb(255, 255, 255);
+            font-weight: bold;
+        }
 
-		ul li a:hover {
-			color: #443020;
-		}
+        ul li a:hover {
+            color: #443020;
+        }
 
-		.form-row button {
-			font-size: 17px;
-			font-family: "workSans";
-			font-weight: bold;
-		}
+        .form-row button {
+            font-size: 17px;
+            font-family: "workSans";
+            font-weight: bold;
+        }
 
-		h1, p, input {
-			font-family: 'workSans';
-		}
-		input {
-			font-weight: bold;
-		}
-	</style>
-  </head>
+        h1, p, input {
+            font-family: 'workSans';
+        }
+        input {
+            font-weight: bold;
+        }
+    </style>
+</head>
 
-  <body>
-    </div>
-      <div class="container h-100">
-  <div class="row h-100 justify-content-center align-items-center">
-    <form action="" method="post">
-     <div class="text-center" style="color: #fff;">
-     <h1>Pharma Management System</h1>
-     </div>
-     <div class="text-center" style="margin-bottom: 5em; color: #fff;">
-     <p>Hola!</p>
-     </div>
-     <div class="form-row justify-content-center">
-       <div class="col-7">
-         <button type="submit" class="btn btn-info btn-block" style="background-color: #36281d; color: #fff;">Admin Portal</button>
-       </div>
-       <div class="col-7 my-1">
-         <button type="submit" class="btn btn-info btn-block" style="background-color: #36281d; color: #fff;">Seller Portal</button>
-       </div>
-      </div>
-
-      </div>
-    </form>
-  </div>
+<body>
 </div>
-  </body>
+<div class="container h-100">
+    <div class="row h-100 justify-content-center align-items-center">
+        <form action="" method="post">
+            <div class="text-center" style="color: #fff;">
+                <h1>Pharma Management System</h1>
+            </div>
+            <div class="text-center" style="margin-bottom: 5em; color: #fff;">
+                <p>Hola!</p>
+            </div>
+            <div class="form-row justify-content-center">
+                <div class="col-7">
+                    <button type="submit" class="btn btn-info btn-block" style="background-color: #36281d; color: #fff;">Admin Portal</button>
+                </div>
+                <div class="col-7 my-1">
+                    <button type="submit" class="btn btn-info btn-block" style="background-color: #36281d; color: #fff;">Seller Portal</button>
+                </div>
+            </div>
+
+    </div>
+    </form>
+</div>
+</div>
+</body>
 </html>
